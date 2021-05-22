07780e4449553804f4dfd3a0c7e54b85e989db20
mehta-saurabh <saurabhmehta33@gmail.com>
2021-05-22 23:46:54 +0200

    initial commit

---
+++ src/test/java/org/rebel/lab/statemachine/StatemachineApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rebel.lab.statemachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatemachineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
